e2dc50dddf0bf50f685f1b1b71217067548f8cc8
OybekBoltaboyev <oybekboltaboyev1443@gmail.com>
2024-07-30 08:10:35 -0400

    Update MAC_changer.py
    Is it too?

--- MAC_changer.py
+++ MAC_changer.py
@@ -1,37 +1,39 @@
-#!/usr/bin/env python
-
-import optparse
 import subprocess
+import argparse
 import re
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interfece change its MAC address")
-    parser.add_option("-m", "--new_mac", dest="new_mac", help="MAC address which will be changed to this ")
-    (options, arguments) = parser.parse_args()
-    if not options.interface:
-        parser.error("[-] Aniq interfeys kiriting.")
-    elif not options.new_mac:
-        parser.error("[-] Aniq MAC manzil kiriting. ")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="MAC manzilini o'zgartirish uchun interfeys")
+    parser.add_argument("-n", "--new_mac", dest="new_mac", help="Yangi MAC manzil")
+    options=parser.parse_args()
+    if not options.new_mac:
+        parser.error("Iltimos yangi MAC manzilni to'g'ri kiriting.")
+    elif not options.interface:
+        parser.error("Iltimos interfeysni to'g'ri kiriting.")
     return options
-
-def change_mac(interface, new_mac):
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
-    subprocess.call(["ifconfig"])
-
-
+def change_mac(interface,new_mac):
+    print("MAC manzil yangisiga o'zgaryapti! ")
+    subprocess.call("ifconfig " + interface +" down", shell=True)
+    subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+    subprocess.call("ifconfig " +interface+ " up", shell=True)
 def get_current_mac(interface):
-    ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-    mac_address_search_result=re.search("\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
-
-    if mac_address_search_result.group(0):
-        return mac_address_search_result.group(0)
+    ifconfig_result=subprocess.check_output(["ifconfig",interface])
+    mac_changer_result=re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w",str(ifconfig_result))
+    if mac_changer_result:
+        return (mac_changer_result.group())
     else:
-        print("[-] Couldn't read MAC address.")
-options= get_arguments()
-current_mac=get_current_mac(options.interface)
-print("Current MAC=" +str(current_mac))
-change_mac(options.interface, options.new_mac)
+        print("[+] MAC manzilni o'qib bo'lmadi")
+def reset_mac(interface):
+    print(f"{interface} interfeysi uchun MAC amnzil dastlabki holatga qaytarilyapti!")
+    subprocess.call(f"ifconfig {interface} down", shell=True)
+options=get_arguments()
 
 current_mac=get_current_mac(options.interface)
+print("joriy MAC = " +str(current_mac))
+change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+answer=input("Tekshirasizmi? (h/y)\n--> ")
+if not answer=='y':
+    subprocess.call("ifconfig " + options.interface, shell=True)
+else:
+    print("e'tiboringiz uchun rahmat")
